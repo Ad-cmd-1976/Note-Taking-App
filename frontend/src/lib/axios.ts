84432a4e4f6dd897cc2e11f1334741ca7dc838ae
Ad-cmd-1976 <adityavishwakarma9025@gmail.com>
2025-09-03 22:45:52 +0530

    Deployment

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance=axios.create({
-    baseURL:import.meta.env.MODE === "development" ? "http://localhost:8080" : import.meta.env.API_URL as string,
+    baseURL:import.meta.env.MODE === "development" ? "http://localhost:8080" : "https://note-taking-app-api-rs5t.onrender.com",
     withCredentials:true
 });
 
